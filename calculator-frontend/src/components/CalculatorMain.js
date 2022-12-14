8c2f76758e4150a4bd5160b95dc8ebe5c5e52337
bhatia-di <bhatia.di@northeastern.edu>
2022-12-14 00:46:18 -0500

    Added collapsible

--- calculator-frontend/src/components/CalculatorMain.js
+++ calculator-frontend/src/components/CalculatorMain.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { CalculatorScreen } from './CalculatorScreen';
 
 export const CalculatorMain = ()  => {
 
@@ -8,9 +9,121 @@ export const CalculatorMain = ()  => {
         <div className='row height-80'>
             <div className='col-12'>
 
-                <div className='border-theme-pink'>
+                {/* <div className='border-theme-pink' style={{borderLeft: "0px"}}> */}
+                <div className='module'>
+                    <span className="display-6 color-pink mt-2">Calculator Main</span>
 
-                    <span className="display-6 color-pink mt-2">CalculatorMain</span>
+                    <CalculatorScreen />
+
+                    <div className='row'>
+                        <div className='col-2'>
+                            <button className="btn color-white">x!</button>
+                        </div>
+                        <div className='col-2'>
+                            <button className="btn color-white">(</button>
+                        </div>
+                        <div className='col-2'>
+                            <button className="btn color-white">)</button>
+                        </div>
+                        <div className='col-2'>
+                            <button className="btn color-white">%</button>
+                        </div>
+                        <div className='col-2'>
+                            <button className="btn color-white">CE</button>
+                        </div>
+                        <div className='col-2'>
+                        </div>
+                    </div>
+
+
+
+                    <div className='row'>
+                        <div className='col-2'>
+                            <button className="btn color-white">ln</button>
+                        </div>
+                        <div className='col-2'>
+                            <button className="btn color-pink hover-white">7</button>
+                        </div>
+                        <div className='col-2'>
+                            <button className="btn color-pink hover-white">8</button>
+                        </div>
+                        <div className='col-2'>
+                            <button className="btn color-pink hover-white">9</button>
+                        </div>
+                        <div className='col-2'>
+                            <button className="btn color-white">/</button>
+                        </div>
+                        <div className='col-2'>
+                            <button className="btn color-white">CE</button>
+                        </div>
+                        <div className='col-2'>
+                        </div>
+                    </div>
+
+
+                    <div className='row'>
+                        <div className='col-2'>
+                            <button className="btn color-white">log</button>
+                        </div>
+                        <div className='col-2'>
+                            <button className="btn color-pink hover-white">4</button>
+                        </div>
+                        <div className='col-2'>
+                            <button className="btn color-pink hover-white">5</button>
+                        </div>
+                        <div className='col-2'>
+                            <button className="btn color-pink hover-white">6</button>
+                        </div>
+                        <div className='col-2'>
+                            <button className="btn color-white">*</button>
+                        </div>
+                        <div className='col-2'>
+                        </div>
+                    </div>
+
+
+
+                    <div className='row'>
+                        <div className='col-2'>
+                            <button className="btn color-white">√</button>
+                        </div>
+                        <div className='col-2'>
+                            <button className="btn color-pink hover-white">1</button>
+                        </div>
+                        <div className='col-2'>
+                            <button className="btn color-pink hover-white">2</button>
+                        </div>
+                        <div className='col-2'>
+                            <button className="btn color-pink hover-white">3</button>
+                        </div>
+                        <div className='col-2'>
+                            <button className="btn color-white">-</button>
+                        </div>
+                        <div className='col-2'>
+                        </div>
+                    </div>
+
+
+
+                    <div className='row'>
+                        <div className='col-2'>
+                            <button className="btn"> </button>
+                        </div>
+                        <div className='col-2'>
+                            <button className="btn color-pink hover-white">0</button>
+                        </div>
+                        <div className='col-2'>
+                            <button className="btn color-pink hover-white">.</button>
+                        </div>
+                        <div className='col-2'>
+                            <button className="btn color-white">=</button>
+                        </div>
+                        <div className='col-2'>
+                            <button className="btn color-white">+</button>
+                        </div>
+                        <div className='col-2'>
+                        </div>
+                    </div>
 
 
                 </div>
---
+++ calculator-frontend/src/components/CalculatorScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const CalculatorScreen = () => {
+
+
+    return (
+        <div className="h-20 m-2 p-2 border-bottom-theme-pink">
+            <span className="display-6 color-pink mt-2">5</span>
+
+        </div>
+    );
+
+};
